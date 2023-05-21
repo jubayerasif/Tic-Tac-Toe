b2066eb3bbacca962a78245bac09cb3c3e3520a8
jubayerasif <jubaerasif101@gmail.com>
2023-05-21 12:58:47 +0600

    Updating

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -10,4 +10,7 @@ function closePlayerConfig() {
 
 function savePlayerConfig(event) {
   event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get('playername');
+  console.log(enteredPlayername);
 }

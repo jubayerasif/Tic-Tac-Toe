af0de056ca30dd738a7c5f2cbef0d2edbe186b5e
jubayerasif <jubaerasif101@gmail.com>
2023-05-21 21:06:21 +0600

    Updating

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -6,6 +6,8 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
+  formELement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = " ";
 }
 
 function savePlayerConfig(event) {
@@ -13,9 +15,10 @@ function savePlayerConfig(event) {
   const formData = new FormData(event.target);
   const enteredPlayername = formData.get("playername").trim(); //'      '=> ''
   // console.log(enteredPlayername);
-  if (!enteredPlayername) { // enteredPlayername === ''
-    errorsOutputElement.textContent = 'Please enter a valid name!';
+  if (!enteredPlayername) {
+    // enteredPlayername === ''
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name!";
     return;
   }
-  
 }

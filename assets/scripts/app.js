b16592ca724b37456987798a69004203ce358b37
jubayerasif <jubaerasif101@gmail.com>
2023-05-22 01:04:36 +0600

    Updated

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,16 +1,16 @@
-const playerConfigOverlayElement = document.getElementById('config-overlay');
-const backdropElement = document.getElementById('backdrop')
-const formELement = document.querySelector('form');
-const errorsOutputElement = document.getElementById('config-errors');
+const playerConfigOverlayElement = document.getElementById("config-overlay");
+const backdropElement = document.getElementById("backdrop");
+const formELement = document.querySelector("form");
+const errorsOutputElement = document.getElementById("config-errors");
 
-const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
-const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
-const cancelConfigBtnElement = document.getElementById('cancel-config-btn')
+const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
+const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
+const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
 
-cancelConfigBtnElement.addEventListener("click", closePlayerConfig)
+cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
 
-formELement.addEventListener('submit', savePlayerConfig);
+formELement.addEventListener("submit", savePlayerConfig);

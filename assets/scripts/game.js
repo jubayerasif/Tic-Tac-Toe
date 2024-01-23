14f0d5dbe5670ecf2eb174cd502fc91e62fd2e9d
jubayerasif <jubaerasif101@gmail.com>
2024-01-24 01:41:21 +0600

    Updated

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -6,7 +6,19 @@ function startNewGame() {
   gameAreaElement.style.display = "block";
 }
 
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+}
+
 function selectGameField(event) {
-  event.target.textContent = players[activePlayer].symbol; // player[0]
-  event.target.classList.add('disabled')
+  if (event.target.tagName !== "LI"){
+    return;
+  }
+    event.target.textContent = players[activePlayer].symbol; // player[0]
+  event.target.classList.add("disabled");
+  switchPlayer();
 }
